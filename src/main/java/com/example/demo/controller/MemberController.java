8cd5a7124c335cd594b048e6e8759d500f0b2d5a
soheeG <121710859+soheeG@users.noreply.github.com>
2023-05-04 15:19:35 +0900

    20230504

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,14 +1,37 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
+
+import com.example.demo.domain.*;
+import com.example.demo.service.*;
 
 @Controller
 @RequestMapping("member")
 public class MemberController {
+	
+	@Autowired
+	private MemberService service;
 
 	@GetMapping("signup")
-	public void addForm() {
+	public void signupForm() {
 		
+	}
+	
+	@PostMapping("signup")
+	public String signupProcess(Member member, RedirectAttributes rttr) {
+	
+		try {
+			service.signup(member);
+			rttr.addFlashAttribute("message", "회원 가입되었습니다.");
+			return "redirect:/list";
+			
+		} catch (Exception e) {
+			rttr.addFlashAttribute("member", member);
+			rttr.addFlashAttribute("message", "회원 가입 중 문제가 발생했습니다.");
+			return "redirect:/member/signup";
+		}
 	}
 }
